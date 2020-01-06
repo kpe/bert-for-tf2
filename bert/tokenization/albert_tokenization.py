032eda1abd998cf07fd978057b8a491db04e1d41
kpe <kpe.git@gmailbox.org>
2020-01-06 11:36:23 +0100

    albert_tokenization - using compat.v1

--- bert/tokenization/albert_tokenization.py
+++ bert/tokenization/albert_tokenization.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2019 The Google Research Authors.
+# Copyright 2018 The Google AI Team Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 # Lint as: python2, python3
 # coding=utf-8
 """Tokenization classes."""
@@ -26,7 +25,8 @@ import re
 import unicodedata
 import six
 from six.moves import range
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+import tensorflow_hub as hub
 import sentencepiece as spm
 
 SPIECE_UNDERLINE = u"▁".encode("utf-8")
@@ -250,6 +250,25 @@ class FullTokenizer(object):
             self.basic_tokenizer = BasicTokenizer(do_lower_case=do_lower_case)
             self.wordpiece_tokenizer = WordpieceTokenizer(vocab=self.vocab)
         self.inv_vocab = {v: k for k, v in self.vocab.items()}
+
+    @classmethod
+    def from_scratch(cls, vocab_file, do_lower_case, spm_model_file):
+        return FullTokenizer(vocab_file, do_lower_case, spm_model_file)
+
+    @classmethod
+    def from_hub_module(cls, hub_module, spm_model_file):
+        """Get the vocab file and casing info from the Hub module."""
+        with tf.Graph().as_default():
+            albert_module = hub.Module(hub_module)
+            tokenization_info = albert_module(signature="tokenization_info",
+                                              as_dict=True)
+            with tf.Session() as sess:
+                vocab_file, do_lower_case = sess.run(
+                    [tokenization_info["vocab_file"],
+                     tokenization_info["do_lower_case"]])
+        return FullTokenizer(
+            vocab_file=vocab_file, do_lower_case=do_lower_case,
+            spm_model_file=spm_model_file)
 
     def tokenize(self, text):
         if self.sp_model:
