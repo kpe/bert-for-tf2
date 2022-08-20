e1e14bb0e20198f85b0d87744ac578a87d8ee08e
noah <noah.8station@gmail.com>
2022-08-20 10:33:34 +0200

    done

--- bert/transformer.py
+++ bert/transformer.py
@@ -6,7 +6,7 @@
 from __future__ import absolute_import, division, print_function
 
 import tensorflow as tf
-from tensorflow.python import keras
+from tensorflow import keras
 
 from params_flow import LayerNormalization
 
@@ -16,21 +16,21 @@ from bert.layer import Layer
 
 class ProjectionLayer(Layer):
     class Params(Layer.Params):
-        hidden_size        = None
-        hidden_dropout     = 0.1
-        initializer_range  = 0.02
-        adapter_size       = None       # bottleneck size of the adapter - arXiv:1902.00751
+        hidden_size = None
+        hidden_dropout = 0.1
+        initializer_range = 0.02
+        adapter_size = None  # bottleneck size of the adapter - arXiv:1902.00751
         adapter_activation = "gelu"
         adapter_init_scale = 1e-3
 
     def _construct(self, **kwargs):
         super()._construct(**kwargs)
-        self.dense      = None
-        self.dropout    = None
+        self.dense = None
+        self.dropout = None
         self.layer_norm = None
 
         self.adapter_down = None
-        self.adapter_up   = None
+        self.adapter_up = None
 
         self.supports_masking = True
 
@@ -38,25 +38,35 @@ class ProjectionLayer(Layer):
     def build(self, input_shape):
         assert isinstance(input_shape, list) and 2 == len(input_shape)
         out_shape, residual_shape = input_shape
-        self.input_spec = [keras.layers.InputSpec(shape=out_shape),
-                           keras.layers.InputSpec(shape=residual_shape)]
+        self.input_spec = [
+            keras.layers.InputSpec(shape=out_shape),
+            keras.layers.InputSpec(shape=residual_shape),
+        ]
 
-        self.dense = keras.layers.Dense(units=self.params.hidden_size,
-                                        kernel_initializer=self.create_initializer(),
-                                        name="dense")
-        self.dropout    = keras.layers.Dropout(rate=self.params.hidden_dropout)
+        self.dense = keras.layers.Dense(
+            units=self.params.hidden_size,
+            kernel_initializer=self.create_initializer(),
+            name="dense",
+        )
+        self.dropout = keras.layers.Dropout(rate=self.params.hidden_dropout)
         self.layer_norm = LayerNormalization(name="LayerNorm")
 
         if self.params.adapter_size is not None:
-            self.adapter_down = keras.layers.Dense(units=self.params.adapter_size,
-                                                   kernel_initializer=tf.keras.initializers.TruncatedNormal(
-                                                       stddev=self.params.adapter_init_scale),
-                                                   activation=self.get_activation(self.params.adapter_activation),
-                                                   name="adapter-down")
-            self.adapter_up   = keras.layers.Dense(units=self.params.hidden_size,
-                                                   kernel_initializer=tf.keras.initializers.TruncatedNormal(
-                                                       stddev=self.params.adapter_init_scale),
-                                                   name="adapter-up")
+            self.adapter_down = keras.layers.Dense(
+                units=self.params.adapter_size,
+                kernel_initializer=tf.keras.initializers.TruncatedNormal(
+                    stddev=self.params.adapter_init_scale
+                ),
+                activation=self.get_activation(self.params.adapter_activation),
+                name="adapter-down",
+            )
+            self.adapter_up = keras.layers.Dense(
+                units=self.params.hidden_size,
+                kernel_initializer=tf.keras.initializers.TruncatedNormal(
+                    stddev=self.params.adapter_init_scale
+                ),
+                name="adapter-up",
+            )
 
         super(ProjectionLayer, self).build(input_shape)
 
@@ -75,24 +85,28 @@ class ProjectionLayer(Layer):
 
 
 class TransformerSelfAttentionLayer(Layer):
-    class Params(ProjectionLayer.Params,
-                 AttentionLayer.Params):
-        hidden_size         = None
-        num_heads           = None
-        hidden_dropout      = None
-        attention_dropout   = 0.1
-        initializer_range   = 0.02
+    class Params(ProjectionLayer.Params, AttentionLayer.Params):
+        hidden_size = None
+        num_heads = None
+        hidden_dropout = None
+        attention_dropout = 0.1
+        initializer_range = 0.02
 
     def _construct(self, **kwargs):
         super()._construct(**kwargs)
         params = self.params
         if params.hidden_size % params.num_heads != 0:
-            raise ValueError("The hidden_size:[{}] is not a multiple of num_heads:[{}]".format(params.hidden_size,
-                                                                                               params.num_heads))
+            raise ValueError(
+                "The hidden_size:[{}] is not a multiple of num_heads:[{}]".format(
+                    params.hidden_size, params.num_heads
+                )
+            )
         self.size_per_head = params.hidden_size // params.num_heads
-        assert params.size_per_head is None or self.size_per_head == params.size_per_head
+        assert (
+            params.size_per_head is None or self.size_per_head == params.size_per_head
+        )
 
-        self.attention_layer     = None
+        self.attention_layer = None
         self.attention_projector = None
 
         self.supports_masking = True
@@ -118,8 +132,10 @@ class TransformerSelfAttentionLayer(Layer):
         #
         # TODO: is it OK to recompute the 3D attention mask in each attention layer
         #
-        attention_head   = self.attention_layer(layer_input, mask=mask, training=training)
-        attention_output = self.attention_projector([attention_head, layer_input], mask=mask, training=training)
+        attention_head = self.attention_layer(layer_input, mask=mask, training=training)
+        attention_output = self.attention_projector(
+            [attention_head, layer_input], mask=mask, training=training
+        )
 
         return attention_output
 
@@ -131,37 +147,40 @@ class SingleTransformerEncoderLayer(Layer):
     See also: https://github.com/tensorflow/tensor2tensor/blob/master/tensor2tensor/models/transformer.py
     """
 
-    class Params(TransformerSelfAttentionLayer.Params,
-                 ProjectionLayer.Params):
-        intermediate_size       = None
+    class Params(TransformerSelfAttentionLayer.Params, ProjectionLayer.Params):
+        intermediate_size = None
         intermediate_activation = "gelu"
 
     def _construct(self, **kwargs):
         super()._construct(**kwargs)
         params = self.params
         if params.hidden_size % params.num_heads != 0:
-            raise ValueError("The hidden_size:[{}] is not a multiple of num_heads:[{}]".format(params.hidden_size,
-                                                                                               params.num_heads))
+            raise ValueError(
+                "The hidden_size:[{}] is not a multiple of num_heads:[{}]".format(
+                    params.hidden_size, params.num_heads
+                )
+            )
         self.size_per_head = params.hidden_size // params.num_heads
 
         self.self_attention_layer = None
-        self.intermediate_layer   = None
-        self.output_projector     = None
+        self.intermediate_layer = None
+        self.output_projector = None
 
         self.supports_masking = True
 
     def build(self, input_shape):
-        self.input_spec = keras.layers.InputSpec(shape=input_shape)  # [B, seq_len, hidden_size]
+        self.input_spec = keras.layers.InputSpec(
+            shape=input_shape
+        )  # [B, seq_len, hidden_size]
 
         self.self_attention_layer = TransformerSelfAttentionLayer.from_params(
-            self.params,
-            name="attention"
+            self.params, name="attention"
         )
         self.intermediate_layer = keras.layers.Dense(
             name="intermediate",
             units=self.params.intermediate_size,
             activation=self.get_activation(self.params.intermediate_activation),
-            kernel_initializer=self.create_initializer()
+            kernel_initializer=self.create_initializer(),
         )
         self.output_projector = ProjectionLayer.from_params(
             self.params,
@@ -173,13 +192,17 @@ class SingleTransformerEncoderLayer(Layer):
     def call(self, inputs, mask=None, training=None):
         layer_input = inputs
 
-        attention_output    = self.self_attention_layer(layer_input, mask=mask, training=training)
+        attention_output = self.self_attention_layer(
+            layer_input, mask=mask, training=training
+        )
 
         # intermediate
         intermediate_output = self.intermediate_layer(attention_output)
 
         # output
-        layer_output = self.output_projector([intermediate_output, attention_output], mask=mask)
+        layer_output = self.output_projector(
+            [intermediate_output, attention_output], mask=mask
+        )
 
         return layer_output
 
@@ -194,15 +217,15 @@ class TransformerEncoderLayer(Layer):
     """
 
     class Params(SingleTransformerEncoderLayer.Params):
-        num_layers     = None
-        out_layer_ndxs = None   # [-1]
+        num_layers = None
+        out_layer_ndxs = None  # [-1]
 
-        shared_layer   = False  # False for BERT, True for ALBERT
+        shared_layer = False  # False for BERT, True for ALBERT
 
     def _construct(self, **kwargs):
         super()._construct(**kwargs)
-        self.encoder_layers   = []
-        self.shared_layer     = None  # for ALBERT
+        self.encoder_layers = []
+        self.shared_layer = None  # for ALBERT
         self.supports_masking = True
 
     def build(self, input_shape):
@@ -211,7 +234,9 @@ class TransformerEncoderLayer(Layer):
         # create all transformer encoder sub-layers
         if self.params.shared_layer:
             # ALBERT: share params
-            self.shared_layer = SingleTransformerEncoderLayer.from_params(self.params, name="layer_shared")
+            self.shared_layer = SingleTransformerEncoderLayer.from_params(
+                self.params, name="layer_shared"
+            )
         else:
             # BERT
             for layer_ndx in range(self.params.num_layers):
@@ -228,7 +253,11 @@ class TransformerEncoderLayer(Layer):
 
         layer_outputs = []
         for layer_ndx in range(self.params.num_layers):
-            encoder_layer = self.encoder_layers[layer_ndx] if self.encoder_layers else self.shared_layer
+            encoder_layer = (
+                self.encoder_layers[layer_ndx]
+                if self.encoder_layers
+                else self.shared_layer
+            )
             layer_input = layer_output
 
             layer_output = encoder_layer(layer_input, mask=mask, training=training)
@@ -244,5 +273,3 @@ class TransformerEncoderLayer(Layer):
             final_output = tuple(final_output)
 
         return final_output
-
-
